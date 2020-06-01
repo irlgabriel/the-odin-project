bc6d1015beeb7516ffa81ec2f778a126067db73d
irlgabriel <radu.gabriel131@gmail.com>
2020-06-01 17:51:54 +0300

    Added restart game option and I am happy rn!

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -3,6 +3,20 @@ let playerScore = 0;
 let computerScore = 0;
 let endGame = false;
 
+function restartGame(){
+  playerScore = 0;
+  computerScore = 0;
+  endGame = false;
+  const result = document.querySelector('#result-div');
+  result.innerHTML = `<p id="round-result"></p>
+  <div id="scores" class="flex-container">
+    <p id="computerScore"></p>
+    <p id="playerScore"></p>
+  </div>`
+  const newgm = document.querySelector('.newGameDiv');
+  newgm.parentNode.removeChild(newgm);
+}
+
 function computerPlay() {
   const r = Math.random();
   return (r <= 0.33) ? 'rock' : (r <= 0.66) ? 'paper' : 'scissors'; 
@@ -28,14 +42,29 @@ function playerWon() {
 const buttons = document.querySelectorAll('button');
 buttons.forEach( (button) => {
   button.addEventListener('click', (e) => {
-    if(endGame == true) return;
-
-    if(computerScore == 5 || playerScore == 5) endGame = true;
+    if(endGame == true) 
+      return;
+    if(computerScore == 5 || playerScore == 5) {
+      endGame = true;
+    }
       else game(button.id);
   })
 })
 
+function createEndGameBtn(){
 
+  const bdy = document.querySelector('body');
+  const newGameDiv = document.createElement('div');
+  newGameDiv.classList.add('newGameDiv');
+  newGameDiv.style.cssText = "text-align: center";
+  const newGame = document.createElement('button');
+  newGame.classList.add('newGameBtn');
+  newGame.textContent = "Start a new Game!";
+  newGame.style.cssText = "padding: 10px 15px; margin-top: 25px";
+  newGameDiv.appendChild(newGame);
+  bdy.appendChild(newGameDiv);
+  return;
+}
 
 function game(player) {
   let computer = computerPlay();
@@ -80,5 +109,13 @@ function game(player) {
   if(computerScore == 5) computerWon();
   if(playerScore == 5) playerWon();
 
+  //c
+  if(computerScore == 5 || playerScore == 5) {
+    createEndGameBtn();
+    const newGameBtn = document.querySelector('.newGameBtn');
+    newGameBtn.addEventListener('click', (e) => {
+      restartGame();
+    })
+  }
 }
 
