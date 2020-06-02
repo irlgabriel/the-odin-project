e16032bfa2f248d0f114fe5f50dcb64578a7e9bd
irlgabriel <radu.gabriel131@gmail.com>
2020-06-03 00:35:36 +0300

    last commit

--- etch-a-sketch/script.js
+++ etch-a-sketch/script.js
@@ -1,25 +1,50 @@
 //16x16 div grid;
-const body = document.querySelector('body');
-body.style.height = '100vh';
-const grid = document.createElement('div');
-grid.classList.add('grid');
-grid.style.height = "100%";
+const body = document.getElementById('body');
+let gridSize = 16;
+let dark = 70;
+createGrid(gridSize);
 
-for(i = 0; i < 15; i ++) {
-  const row = document.createElement('div');
-  row.classList.add('row');
-  row.style.cssText = 'margin: 0 auto';
-  row.style.height = "6%";
+function getRandomColor() {
+  let rand1 = Math.floor(Math.random() * 360);
 
-  for(j = 0; j < 15; j ++) {
-    const cell = document.createElement('div')
-    cell.style.display = "inline-block";
-    cell.style.width = "6%";
-    cell.style.height = "6%";
-    row.appendChild(cell);
-  }
-
-  grid.appendChild(row);
+  return `hsl(${rand1}, 100%, ${dark}%)`;
 }
 
-body.appendChild(grid);
+function clearGrid() {
+  const rows = document.querySelectorAll('.row');
+  for(row of [...rows]) {
+    row.parentNode.removeChild(row);
+  }
+}
+
+function createGrid(gridSize) {
+  for(i = 0; i < gridSize; i ++) {
+    const row = document.createElement('div');
+    row.classList.add('row')
+    row.style.cssText = `display: flex; height: ${99/gridSize}%; width: 100%;`;
+    for(j = 0; j < gridSize; j ++) {
+      const cell = document.createElement('div');
+      cell.classList.add('cell');
+      cell.style.cssText = `border: 1px solid red; flex: 1; height: 100%; width: ${99/gridSize}% ; background-color: white;`;
+
+      cell.addEventListener('mouseover', () => {
+        cell.style.backgroundColor = `${getRandomColor()}`;
+  
+      })
+
+      row.appendChild(cell);
+    }
+    body.appendChild(row);
+  }
+}
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', (e) => {
+  const cells = document.querySelectorAll('.cell');
+  for(cell of [...cells]) {
+    cell.style.backgroundColor = 'white';
+  }
+  gridSize = prompt('enter grid size: ');
+  clearGrid();
+  createGrid(gridSize);
+})
