52815d256b8bb0499306b32941dc2909a6d7f94b
irlgabriel <radu.gabriel131@gmail.com>
2020-06-13 12:54:05 +0300

    fixed

--- recursion/mergesort.rb
+++ recursion/mergesort.rb
@@ -1,48 +1,35 @@
 require 'pry'
 
-def merge(arr, left, mid, right) 
-  l = arr.slice(left, mid)
-  r = arr.slice(mid, right)
-
-  k = left
-  i = 0
-  j = 0
-
-  while i < l.length && j < r.length do
-    if l[i] <= r[j]
-      arr[k] = l[i]
-      i += 1
+def merge(left_arr, right_arr)
+  arr = []
+  until left_arr.empty? || right_arr.empty?
+    if left_arr.first <= right_arr.first
+      arr << left_arr.shift
     else
-      arr[k] = r[j]
-      j += 1
+      arr << right_arr.shift
     end
-    k += 1
   end
-  
-  while i < l.length do
-    arr[k] = l[i]
-    i += 1
-    k += 1
-  end
-  
-  while j < r.length do
-    arr[k] = r[j]
-    j += 1
-    k += 1
-  end
-
+  arr << left_arr.shift until left_arr.empty?
+  arr << right_arr.shift until right_arr.empty?
+  arr
 end
 
-def mergesort(arr, left, right)
-  if left < right
-    mid = (left + right) / 2
-    mergesort(arr, left, mid)
-    mergesort(arr, mid + 1, right)
-    merge(arr, left, mid, right)
+def merge_sort(arr)
+  if arr.length >= 2
+    mid = arr.length / 2
+    left = merge_sort(arr.take(mid))
+    right = merge_sort(arr.drop(mid))
+    merge(left, right)
+  else
+    return arr
   end
 end
 
-arr = [5, 0, 1, 0, 4, 12]
-p arr
-mergesort(arr, 0, arr.length - 1)
-p arr
+
+array = []
+10.times do
+  array << rand(100)
+end
+
+p array
+p merge_sort(array)
